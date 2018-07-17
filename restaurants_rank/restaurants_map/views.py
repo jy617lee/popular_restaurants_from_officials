10a9e05be7c8f4b9b63fa1b8a4492cf9dd6e67ea
jee yun lee <jy617.lee@gmail.com>
2018-07-17 18:12:06 +0900

    예쁘게 만들었당!

--- restaurants_rank/restaurants_map/views.py
+++ restaurants_rank/restaurants_map/views.py
@@ -7,7 +7,9 @@ def input_email(request):
     if request.method == "POST":
         form = EmailForm(request.POST)
         if form.is_valid():
+            email_text = request.POST['input_email']
             email = form.save(commit=False)
+            email.email_addr = email_text
             email.register_date = timezone.now()
             email.save()
         return render(request, 'restaurants_map/complete_email.html', {})
