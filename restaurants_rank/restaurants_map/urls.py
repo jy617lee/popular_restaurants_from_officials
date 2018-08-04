77a1a123e853afea77a4995c2f65ae9b3dc454fa
jee yun lee <jy617.lee@gmail.com>
2018-08-04 23:00:25 +0900

    url 수정, css 수정

--- restaurants_rank/restaurants_map/urls.py
+++ restaurants_rank/restaurants_map/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^input_email$', views.input_email, name='input_email'),
     url(r'^dining_list$', views.dining_list, name='dining_list'),
-    url(r'^dining_list_man$', views.dining_list, name='dining_list'),
-    url(r'^dining_list_women$', views.dining_list, name='dining_list'),
+    url(r'^dining_list_m$', views.dining_list, name='dining_list'),
+    url(r'^dining_list_w$', views.dining_list, name='dining_list'),
 ]
