c779734df8b9d4018386babdea773b148ffb6e20
jiyoung-choi <jiyoung031@gmail.com>
2018-08-04 16:17:42 +0900

    Fixed #10

--- restaurants_rank/restaurants_map/urls.py
+++ restaurants_rank/restaurants_map/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^djga/', include('google_analytics.urls')),
     url(r'^input_email$', views.input_email, name='input_email'),
     url(r'^dining_list$', views.dining_list, name='dining_list'),
     url(r'^dining_list_man$', views.dining_list, name='dining_list'),
