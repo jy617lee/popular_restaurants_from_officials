0fb52b22db260570b6937cfb123c1ddab06f0350
jee yun lee <jy617.lee@gmail.com>
2018-08-06 11:04:26 +0900

    url modify :dining_list --> official jmt

--- restaurants_rank/restaurants_map/urls.py
+++ restaurants_rank/restaurants_map/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^input_email$', views.input_email, name='input_email'),
-    url(r'^dining_list$', views.dining_list, name='dining_list'),
-    url(r'^dining_list_m$', views.dining_list, name='dining_list'),
-    url(r'^dining_list_w$', views.dining_list, name='dining_list'),
+    url(r'^official_jmt$', views.dining_list, name='dining_list'),
+    url(r'^official_jmt_m$', views.dining_list, name='dining_list'),
+    url(r'^official_jmt_w$', views.dining_list, name='dining_list'),
 ]
