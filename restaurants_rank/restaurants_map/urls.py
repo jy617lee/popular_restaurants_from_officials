880d87649320dc012678864712eabac82a2d3d08
jee yun lee <jy617.lee@gmail.com>
2018-08-06 21:39:18 +0900

    resolve conflicts

--- restaurants_rank/restaurants_map/urls.py
+++ restaurants_rank/restaurants_map/urls.py
@@ -3,13 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^input_email$', views.input_email, name='input_email'),
-<<<<<<< HEAD
     url(r'^official_jmt$', views.dining_list, name='dining_list'),
     url(r'^official_jmt_m$', views.dining_list, name='dining_list'),
     url(r'^official_jmt_w$', views.dining_list, name='dining_list'),
-=======
-    url(r'^dining_list$', views.dining_list, name='dining_list'),
-    url(r'^dining_list_m$', views.dining_list, name='dining_list'),
-    url(r'^dining_list_w$', views.dining_list, name='dining_list'),
->>>>>>> url 수정, css 수정
 ]
